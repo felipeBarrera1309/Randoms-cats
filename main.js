d31ebf673636cfde2656279346222a35c79e5f09
felipeBarrera <felipebarrera.gbp@gmail.com>
2023-05-31 21:24:56 -0500

    agregar gatitos dinamicamente y marcar como favorito

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const API_KEY = 'live_86eNcoc9sFOy4nSDHZlAczskKAiJO4dbWvkg2nkl2TyP6Q9xXquSlv3E1i
 
 const API_URL = `https://api.thecatapi.com/v1/images/search?limit=2&api_key=${API_KEY}`;
 
-const API_URL_POST = `https://api.thecatapi.com/v1/favourites?limit=2&api_key=${API_KEY}`
+const API_URL_POST = `https://api.thecatapi.com/v1/favourites?sub_id=user-123&api_key=${API_KEY}`
+
+const API_URL_DELETE = `https://api.thecatapi.com/v1/favourites/`
 
 const button = document.querySelector('#button-reaload')
 
@@ -20,8 +22,8 @@ let urlTwo = null
 let urlThree = null
 
 let datasIdis = {
-    0: { id: null },
-    1: { id: null },
+    0: { id: null, url: null },
+    1: { id: null, url: null },
 }
 
 let saveDataMichis = []
@@ -39,7 +41,7 @@ async function reload(){
         imageTwo.setAttribute('src', urlTwo);
         saveDataMichis = seeData
         seeData.forEach((el, i) => {
-            datasIdis[i] = { id: el.id }
+            datasIdis[i] = { id: el.id, url: el.url }
         })
     }catch(error){
         if(error.message){
@@ -56,37 +58,51 @@ reload()
 const buttonOne = document.querySelector('#button-one')
 const buttonTwo = document.querySelector('#button-two')
 
-function saveMichisFavourites(position){
+async function saveMichisFavourites(position){
     const parentDiv = document.querySelector('.parent-save')
-    saveDataMichis.forEach(michi => {
-        const article = document.createElement('article');
-        const image = document.createElement('img')
-        const btn = document.createElement('button')
-        btn.textContent = 'Sacar el michi de favotiros'
-        image.setAttribute('src', michi.url)
+    const article = document.createElement('article')
+    const image = document.createElement('img')
+    const btn = document.createElement('button')
+    btn.textContent = 'Favourtie cat <3'
+    image.setAttribute('src', datasIdis[position].url)
+    article.id = datasIdis[position].id
+
+
+    try{
+        const request = await fetch(API_URL_POST, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+                image_id: 'db4'
+            })
+        })
+        const data = await request.json()
+        console.log('Esta es la data: ', data);
+
         parentDiv.appendChild(article)
         article.appendChild(image)
         article.appendChild(btn)
-        console.log(parentDiv);
-    })
+
+
+    }catch(error){
+        console.log('Este es el error traido en pantalla: ', error.message);
+    }
+
+    btn.onclick = () => deleteMichisFavourites(datasIdis[position].id);
 
 }
 
-async function saveFavouritesCats(value){
+async function deleteMichisFavourites(id){
+
+    let newArticle = document?.querySelector(`#${id}`)
+    const getIdi = newArticle.getAttribute('id')
+    console.log('id obtenido: ', getIdi);
+
     try{
-        const res = await fetch(API_URL_POST, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                image_id: '31p'
-            })
+        const res = await fetch(`${API_URL_DELETE+id}?api_key=${API_KEY}`, {
+            method: 'DELETE',
         })
-        const data = await res.json()
-        console.log('Datos satisfactoriamente recibidos: ', data);
-    }
-    catch(error){s
-        console.log(error.message);
+    }catch(error){
+        console.log('Este es el error recibido: ', error.message);
     }
 }
